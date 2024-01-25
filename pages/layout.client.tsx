cf4ab322db7f1c595386cb753f0558ac6693cf53
StupidBeLike <yanborovyk@gmail.com>
2024-01-25 01:18:09 +0100

    Final change before personal project

--- pages/layout.client.tsx
+++ pages/layout.client.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import styles from './stylesheet.module.css';
 import '../styles/global.css';
@@ -7,66 +6,61 @@ import myImage2 from './test2.png';
 import myImage3 from './test3.png';
 import myImage4 from './test4.png';
 import myImage5 from './test5.png';
+import title from './title.png';
+import header from './header.png';
 
 const Page: React.FC = () => {
-  const [imagePosition1, setImagePosition1] = useState({ x: 2100, y: 500 });
-  const [imagePosition2, setImagePosition2] = useState({ x: 2100, y: 500 });
-  const [imagePosition3, setImagePosition3] = useState({ x: 2100, y: 500 });
-  const [imagePosition4, setImagePosition4] = useState({ x: 2100, y: 500 });
-  const [imagePosition5, setImagePosition5] = useState({ x: 2100, y: 500 });
+  const initialPosition = { x: 2100, y: 250 };
+  const [imagePositions, setImagePositions] = useState({
+    image1: initialPosition,
+    image2: initialPosition,
+    image3: initialPosition,
+    image4: initialPosition,
+    image5: initialPosition,
+  });
+  const [activeImage, setActiveImage] = useState(null);
 
-  const handleContainerClick1 = () => {
-    setImagePosition1({ x: 1000, y: 500 });
-  };
-
-  const handleContainerClick2 = () => {
-    setImagePosition2({ x: 1000, y: 500 });
-  };
-
-  const handleContainerClick3 = () => {
-    setImagePosition3({ x: 1000, y: 500 });
-  };
-
-  const handleContainerClick4 = () => {
-    setImagePosition4({ x: 1000, y: 500 });
-  };
-
-  const handleContainerClick5 = () => {
-    setImagePosition5({ x: 1000, y: 500 });
+  const handleContainerClick = (imageKey: string, newPosition: { x: number, y: number }) => {
+    setImagePositions(prevPositions => ({
+      ...prevPositions,
+      [activeImage as unknown as string]: initialPosition, // reset the position of the active image
+      [imageKey]: newPosition, // set the new position for the clicked image
+    }));
+    setActiveImage(imageKey as string | null); // set the new active image
   };
 
   return (
     <body>
-      <img className={styles.image} src={myImage1.src} alt="description" style={{ position: 'absolute', left: `${imagePosition1.x}px`, top: `${imagePosition1.y}px`, transition: 'all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)' }} />
-      <img className={styles.image} src={myImage2.src} alt="description" style={{ position: 'absolute', left: `${imagePosition2.x}px`, top: `${imagePosition2.y}px`, transition: 'all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)' }} />
-      <img className={styles.image} src={myImage3.src} alt="description" style={{ position: 'absolute', left: `${imagePosition3.x}px`, top: `${imagePosition3.y}px`, transition: 'all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)' }} />
-      <img className={styles.image} src={myImage4.src} alt="description" style={{ position: 'absolute', left: `${imagePosition4.x}px`, top: `${imagePosition4.y}px`, transition: 'all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)' }} />
-      <img className={styles.image} src={myImage5.src} alt="description" style={{ position: 'absolute', left: `${imagePosition5.x}px`, top: `${imagePosition5.y}px`, transition: 'all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)' }} />
+      <img className={`${styles.image} ${styles.image1}`} src={myImage1.src} alt="description" style={{ position: 'absolute', left: `${imagePositions.image1.x}px`, top: `${imagePositions.image1.y}px`, transition: 'all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)' }} />
+      <img className={`${styles.image} ${styles.image2}`} src={myImage2.src} alt="description" style={{ position: 'absolute', left: `${imagePositions.image2.x}px`, top: `${imagePositions.image2.y}px`, transition: 'all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)' }} />
+      <img className={`${styles.image} ${styles.image3}`} src={myImage3.src} alt="description" style={{ position: 'absolute', left: `${imagePositions.image3.x}px`, top: `${imagePositions.image3.y}px`, transition: 'all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)' }} />
+      <img className={`${styles.image} ${styles.image4}`} src={myImage4.src} alt="description" style={{ position: 'absolute', left: `${imagePositions.image4.x}px`, top: `${imagePositions.image4.y}px`, transition: 'all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)' }} />
+      <img className={`${styles.image} ${styles.image5}`} src={myImage5.src} alt="description" style={{ position: 'absolute', left: `${imagePositions.image5.x}px`, top: `${imagePositions.image5.y}px`, transition: 'all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)' }} />
+      <div className={styles.box}>
+        <div className={`${styles.container} ${styles.examples}`} onClick={() => handleContainerClick('image1', { x: 1000, y: 250 })}>
+          <div className={styles.containername}>CALENDAR</div>
+        </div>
 
+        <div className={`${styles.container} ${styles.features}`} onClick={() => handleContainerClick('image2', { x: 1000, y: 250 })}>
+          <div className={styles.containername}>CLASSES</div>
+        </div>
 
-<div className={styles.box}>
-    <div className={`${styles.container} ${styles.examples}`} onClick={handleContainerClick1}>
-      <div className={styles.containername}>CALENDAR</div>
-    </div>
+        <div className={`${styles.container} ${styles.pricing}`} onClick={() => handleContainerClick('image3', { x: 1000, y: 250 })}>
+          <div className={styles.containername}>GRADES</div>
+        </div>
 
-    <div className={`${styles.container} ${styles.features}`} onClick={handleContainerClick2}>
-      <div className={styles.containername}>CLASSES</div>
-    </div>
+        <div className={`${styles.container} ${styles.contact}`} onClick={() => handleContainerClick('image4', { x: 1000, y: 250 })}>
+          <div className={styles.containername}>ADMIN</div>
+        </div>
 
-    <div className={`${styles.container} ${styles.pricing}`} onClick={handleContainerClick3}>
-      <div className={styles.containername}>GRADES</div>
-    </div>
+        <div className={`${styles.container} ${styles.other}`} onClick={() => handleContainerClick('image5', { x: 1000, y: 250 })}>
+          <div className={styles.containername}>OTHER</div>
+        </div>
+      </div>
+      <div className={styles.gynged}> GYNGED</div>
+      <img className={`${styles.title} ${styles.title}`} src={title.src} alt="Title" />
+      <img className={`${styles.header} ${styles.header}`} src={header.src} alt="header" />
 
-    <div className={`${styles.container} ${styles.contact}`} onClick={handleContainerClick4}>
-      <div className={styles.containername}>ADMIN</div>
-    </div>
-
-    <div className={`${styles.container} ${styles.other}`} onClick={handleContainerClick5}>
-      <div className={styles.containername}>OTHER</div>
-    </div>
-    </div>
-    
-    <div className={styles.gynged}> GYNGED</div>
     </body>
   );
 };
